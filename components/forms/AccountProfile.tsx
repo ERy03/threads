f60b4bb6bcbc1c3b3ce8d092c0a2800b03ed0e6d
Ryota <ryoenoki2013@gmail.com>
2023-12-02 17:52:45 +0100

    update: form bio section

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,6 +17,7 @@ import { UserValidation } from "@/lib/validations/user";
 import * as z from "zod";
 import Image from "next/image";
 import { ChangeEvent } from "react";
+import { Textarea } from "@/components/ui/textarea";
 
 interface Props {
   user: {
@@ -140,7 +141,25 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
           )}
         />
 
-        {/* //TODO bio */}
+        {/* bio */}
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem className="flex items-center gap-3 w-full">
+              <FormLabel className="text-base-semibold text-light-2">
+                Username
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <Textarea
+                  rows={10}
+                  className="account-form_input no-focus"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
 
         <Button type="submit">Submit</Button>
       </form>
