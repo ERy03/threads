1a7c065735ab9e9a4edb18fdb5c6e3463ce35f15
Ryota <ryoenoki2013@gmail.com>
2023-12-04 17:11:02 +0100

    update: default values

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -35,11 +35,10 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
-      // TODO
-      profile_photo: "",
-      name: "",
-      username: "",
-      bio: "",
+      profile_photo: user?.image || "",
+      name: user?.name || "",
+      username: user?.username || "",
+      bio: user?.bio || "",
     },
   });
 
