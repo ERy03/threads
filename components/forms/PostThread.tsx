6e5139d94859b81067ba75aa257861d6379499c5
Ryota <ryoenoki2013@gmail.com>
2023-12-13 00:06:50 +0100

    update: form with validation wip

--- components/forms/PostThread.tsx
+++ components/forms/PostThread.tsx
@@ -1,3 +1,45 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Textarea } from "@/components/ui/textarea";
+import { usePathname, useRouter } from "next/navigation";
+
+import { updateUser } from "@/lib/actions/user.actions";
+import { ThreadValidation } from "@/lib/validations/thread";
+interface Props {
+  user: {
+    id: string;
+    objectId: string;
+    username: string;
+    name: string;
+    bio: string;
+    image: string;
+  };
+  btnTitle: string;
+}
+
 export default function PostThread({ userId }: { userId: string }) {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const form = useForm({
+    resolver: zodResolver(ThreadValidation),
+    defaultValues: {
+      thread: "",
+      accountId: userId,
+    },
+  });
   return <h1>Post Thread Form</h1>;
 }
