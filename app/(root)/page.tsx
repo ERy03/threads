d58d4168d96856c7d4eed126cceefc168658dd6e
Ryota <ryoenoki2013@gmail.com>
2023-12-18 17:07:33 +0100

    remove: comment

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,6 @@ import { currentUser } from "@clerk/nextjs";
 export default async function Home() {
   const result = await fetchPosts(1, 30);
   const user = await currentUser();
-  console.log(result);
 
   return (
     <>
