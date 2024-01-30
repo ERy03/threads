86a6c148166f1f2df7ecab982d7553a7550a1812
Ryota <ryoenoki2013@gmail.com>
2024-01-30 23:44:05 +0100

    update: use btnTitle

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -210,7 +210,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         />
 
         <Button type="submit" className="bg-primary-500">
-          Submit
+          {btnTitle}
         </Button>
       </form>
     </Form>
